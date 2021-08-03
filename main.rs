408281c1fe51d0a3d705e9d24ba7c6c251acfdf8
Attila Simon <sati290@gmail.com>
2021-08-03 11:17:38 +0200

    precompute camera rays

--- main.rs
+++ main.rs
@@ -5,17 +5,8 @@ use std::time::Instant;
 
 type Vector2 = na::Vector2<f32>;
 type Vector3 = na::Vector3<f32>;
-
-struct Ray {
-    origin: Vector3,
-    direction: na::Unit<Vector3>,
-}
-
-impl Ray {
-    fn new(origin: Vector3, direction: na::Unit<Vector3>) -> Ray {
-        Ray { origin, direction }
-    }
-}
+type UVector3 = na::Unit<Vector3>;
+type Matrix4 = na::Matrix4<f32>;
 
 struct Sphere {
     center: Vector3,
@@ -24,10 +15,10 @@ struct Sphere {
 }
 
 impl Sphere {
-    fn intersect(&self, r: &Ray) -> Option<f32> {
+    fn intersect(&self, ray_origin: &Vector3, ray_direction: &UVector3) -> Option<f32> {
         let r2 = self.radius * self.radius;
-        let l = self.center - r.origin;
-        let tca = l.dot(&r.direction);
+        let l = self.center - ray_origin;
+        let tca = l.dot(&ray_direction);
         let d2 = l.dot(&l) - tca * tca;
 
         if d2 > r2 {
@@ -54,7 +45,7 @@ fn color_vec_to_rgb(v: Vector3) -> image::Rgb<u8> {
     image::Rgb([(v.x * 255.) as u8, (v.y * 255.) as u8, (v.z * 255.) as u8])
 }
 
-fn main() {
+fn generate_camera_rays(image_width: u32, image_height: u32, horiz_fog_deg: f32) -> Vec<(u32, u32, Vec<UVector3>)> {
     let subpixels = [
         Vector2::new(5. / 8., 1. / 8.),
         Vector2::new(1. / 8., 3. / 8.),
@@ -62,6 +53,30 @@ fn main() {
         Vector2::new(3. / 8., 7. / 8.),
     ];
 
+    let aspect = image_height as f32 / image_width as f32;
+    let plane_dist = (horiz_fog_deg.to_radians() / 2.).tan();
+    let image_dims_recip = Vector2::new(1. / image_width as f32, 1. / image_height as f32);
+
+    let mut rays = Vec::<(u32, u32, Vec::<UVector3>)>::with_capacity((image_width * image_height) as usize);
+    for x in 0..image_width {
+        for y in 0..image_height {
+            let mut sp_rays = Vec::<UVector3>::with_capacity(subpixels.len());
+            
+            let px = Vector2::new(x as f32, (image_height - y) as f32);
+            for sp in subpixels {
+                let px = (px + sp).component_mul(&image_dims_recip) - Vector2::new(0.5, 0.5);
+                let ray = UVector3::new_normalize(Vector3::new(px.x, px.y * aspect, plane_dist));
+                sp_rays.push(ray);
+            }
+
+            rays.push((x, y, sp_rays));
+        }
+    }
+
+    rays
+}
+
+fn main() {
     let scene = [
         Sphere {
             center: Vector3::new(0., 0., 0.),
@@ -80,54 +95,50 @@ fn main() {
     let image_height = 1080;
     let mut image = image::RgbImage::new(image_width, image_height);
 
-    let fov_deg: f32 = 90.;
-    let plane_dist = (fov_deg.to_radians() / 2.).tan();
-    let aspect = image_height as f32 / image_width as f32;
-    let image_dims = Vector2::new(image_width as f32, image_height as f32);
-    println!(
-        "{} pixels, {} samples",
-        image_width * image_height,
-        image_width * image_height * subpixels.len() as u32
-    );
+    let camera_rays = generate_camera_rays(image_width, image_height, 90.);
 
     let time_start = Instant::now();
 
-    for (x, y, pixel) in image.enumerate_pixels_mut() {
-        let mut color = Vector3::zeros();
-        let px = Vector2::new(x as f32, (image_height - y) as f32);
-        for sp in subpixels {
-            let px = (px + sp).component_div(&image_dims) - Vector2::new(0.5, 0.5);
-            let px = Vector3::new(px.x, px.y * aspect, plane_dist);
-            let ray = Ray::new(cam_pos, na::Unit::<Vector3>::new_normalize(px));
+    let frames = 50;
+    for _ in 0..frames {
+        for (x, y, rays) in &camera_rays {
+            let mut color = Vector3::zeros();
+            for r in rays {
+                let mut closest_hit: Option<(&Sphere, f32)> = None;
+                for o in &scene {
+                    if let Some(hit) = o.intersect(&cam_pos, &r) {
+                        let closest = match closest_hit {
+                            Some(c) => hit < c.1,
+                            None => true,
+                        };
 
-            let mut closest_hit: Option<(&Sphere, f32)> = None;
-            for o in &scene {
-                if let Some(hit) = o.intersect(&ray) {
-                    let closest = match closest_hit {
-                        Some(c) => hit < c.1,
-                        None => true,
-                    };
-
-                    if closest {
-                        closest_hit = Some((o, hit));
+                        if closest {
+                            closest_hit = Some((o, hit));
+                        }
                     }
+                }
+
+                if let Some(hit) = closest_hit {
+                    let hit_pos = cam_pos + r.into_inner() * hit.1;
+                    let normal = na::Unit::<Vector3>::new_normalize(hit_pos - hit.0.center);
+                    let light_dir = na::Unit::<Vector3>::new_normalize(light_pos - hit_pos);
+                    let ndl = light_dir.dot(&normal);
+
+                    color += hit.0.color * ndl / rays.len() as f32;
                 }
             }
 
-            if let Some(hit) = closest_hit {
-                let hit_pos = ray.origin + ray.direction.into_inner() * hit.1;
-                let normal = na::Unit::<Vector3>::new_normalize(hit_pos - hit.0.center);
-                let light_dir = na::Unit::<Vector3>::new_normalize(light_pos - hit_pos);
-                let ndl = light_dir.dot(&normal);
-
-                color += hit.0.color * ndl / subpixels.len() as f32;
-            }
+            image.put_pixel(*x, *y, color_vec_to_rgb(color));
         }
-
-        *pixel = color_vec_to_rgb(color);
     }
 
-    println!("Elapsed time: {:.2?}", time_start.elapsed());
+    let elapsed = time_start.elapsed();
+    println!(
+        "{:.2?} for {} frames, {:.2?}/frame",
+        elapsed,
+        frames,
+        elapsed / frames
+    );
 
     image.save("output.png").unwrap();
 }
